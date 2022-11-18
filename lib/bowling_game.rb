952e57ce526fc97c44051a5e486afb7a2496c91a
Iain <iain.hoolahan@gmail.com>
2022-11-18 14:23:01 +0000

    user_input_scores method tested for basic score scenarios

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,2 +1,54 @@
 # file: lib/bowling_game.rb
 
+class Bowling
+
+  def initialize(io)
+    @score_card = []
+    @io = io
+  end
+
+  def user_input_scores
+    frame_score = []
+    # first roll
+    while true do
+      @io.puts "Please enter first roll score:"
+      first_roll = @io.gets.chomp.to_i
+      if first_roll == 10
+        @io.puts "STRIKE!"
+        frame_score << first_roll
+        break
+      elsif
+        first_roll < 11 && first_roll >=0
+        frame_score << first_roll
+        break
+      elsif first_roll > 10
+        @io.puts "Invalid score: maximum 10 on first roll"
+      end
+    end
+    # second roll
+    while true do
+      if first_roll == 10
+        second_roll = 0
+        break
+      else
+        @io.puts "Please enter second roll score:"
+        second_roll = @io.gets.chomp.to_i
+        if first_roll + second_roll == 10
+          @io.puts "SPARE!"
+          frame_score << second_roll
+          break
+        elsif (first_roll + second_roll) < 11 && second_roll >=0
+          frame_score << second_roll
+          break
+        elsif (first_roll + second_roll) > 10
+          @io.puts "Invalid score: maximum #{10 - first_roll} on second roll"
+        end
+      end
+    end
+    @io.puts "Frame score: #{frame_score.sum}"
+  end
+
+
+end
+
+# Bowling.new(Kernel).user_input_scores
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,1 +1,55 @@
 # file: spec/bowling_game_spec.rb
+
+require 'bowling_game'
+
+describe Bowling do
+
+  context 'user_input_scores method' do
+    it 'prints messages to the user to collect scores and stores the inputs in an array' do
+      io = double :io
+      expect(io).to receive(:puts).with("Please enter first roll score:").ordered
+      expect(io).to receive(:gets).and_return("4").ordered
+      expect(io).to receive(:puts).with("Please enter second roll score:").ordered
+      expect(io).to receive(:gets).and_return("4").ordered
+      expect(io).to receive(:puts).with("Frame score: 8").ordered
+      game = Bowling.new(io)
+      game.user_input_scores
+    end
+    it 'prints a strike message if the user scores a 10 on the first go' do
+      io = double :io
+      expect(io).to receive(:puts).with("Please enter first roll score:").ordered
+      expect(io).to receive(:gets).and_return("10").ordered
+      expect(io).to receive(:puts).with("STRIKE!").ordered
+      expect(io).to receive(:puts).with("Frame score: 10").ordered
+      game = Bowling.new(io)
+      game.user_input_scores
+    end
+    it 'prints a message if the user inputs an invalid score on the first go' do
+      io = double :io
+      expect(io).to receive(:puts).with("Please enter first roll score:").ordered
+      expect(io).to receive(:gets).and_return("11").ordered
+      expect(io).to receive(:puts).with("Invalid score: maximum 10 on first roll").ordered
+      expect(io).to receive(:puts).with("Please enter first roll score:").ordered
+      expect(io).to receive(:gets).and_return("10").ordered
+      expect(io).to receive(:puts).with("STRIKE!").ordered
+      expect(io).to receive(:puts).with("Frame score: 10").ordered
+      game = Bowling.new(io)
+      game.user_input_scores
+    end
+    it 'prints a message if the user inputs an invalid score on the second go' do
+      io = double :io
+      expect(io).to receive(:puts).with("Please enter first roll score:").ordered
+      expect(io).to receive(:gets).and_return("5").ordered
+      expect(io).to receive(:puts).with("Please enter second roll score:").ordered
+      expect(io).to receive(:gets).and_return("6").ordered
+      expect(io).to receive(:puts).with("Invalid score: maximum 5 on second roll").ordered
+      expect(io).to receive(:puts).with("Please enter second roll score:").ordered
+      expect(io).to receive(:gets).and_return("2").ordered
+      expect(io).to receive(:puts).with("Frame score: 7").ordered
+      game = Bowling.new(io)
+      game.user_input_scores
+    end
+  end
+
+
+end
